621a8ba8a5d5688b54cb393cb55d4fe45296c784
JinalkaHerath <129472608+JinalkaHerath@users.noreply.github.com>
2025-10-29 23:20:17 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MochiRobotStore from './MochiRobotStore.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MochiRobotStore />
+  </React.StrictMode>,
+)
